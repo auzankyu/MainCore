99fec6248bcdbf14992b3ba1dd3c4adbe72cd14e
Auzan Lazuardi <auzanlazuardi@gmail.com>
2024-02-29 16:40:37 +0700

    initial commit

---
+++ MainCore/MainCore.swift
@@ -0,0 +1,16 @@
+//
+//  MainCore.swift
+//  MainCore
+//
+//  Created by Auzan Lazuardi on 29/02/24.
+//
+
+import Foundation
+
+public class MainCore {
+    public static let shared = MainCore()
+    
+    public func test() {
+        print("MainCore")
+    }
+}
